d805dbcd8b4c6453e52c1958ff6aced66e37e87f
Pedro F. Albanese <68971450+pedroalbanese@users.noreply.github.com>
2023-10-30 14:30:54 -0300

    RFC 4543 GMAC (NIST SP800-38D)

--- gmac.go
+++ gmac.go
@@ -7,7 +7,7 @@ import (
 // CalculateGMAC calculates the Galois Message Authentication Code (GMAC)
 func CalculateGMAC(block cipher.Block, nonce, data []byte) ([]byte, error) {
 	// Create an AEAD using the provided block
-	aead, err := cipher.NewGCM(block)
+	aead, err := cipher.NewGCMWithNonceSize(block, 16)
 	if err != nil {
 		return nil, err
 	}
