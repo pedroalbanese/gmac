450d9a50b5ccaf3b9538e724ce2d16f852fdf843
Pedro F. Albanese <68971450+pedroalbanese@users.noreply.github.com>
2023-10-30 14:20:01 -0300

    Add files via upload

--- gmac.go
+++ gmac.go
@@ -5,8 +5,14 @@ import (
 )
 
 // CalculateGMAC calculates the Galois Message Authentication Code (GMAC)
-func CalculateGMAC(block cipher.Block, nonce, data []byte) []byte {
-	mode := cipher.NewGCM(block)
-	gmac := mode.Seal(nil, nonce, data, nil)
-	return gmac
+func CalculateGMAC(block cipher.Block, nonce, data []byte) ([]byte, error) {
+	// Create an AEAD using the provided block
+	aead, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	// Calculate the GMAC using the AEAD object
+	gmac := aead.Seal(nil, nonce, nil, data)
+	return gmac, nil
 }
